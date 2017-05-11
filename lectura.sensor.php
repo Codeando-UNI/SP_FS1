5086f7ada246cbad96b9d0b92702076ad8585927
juan <jotabeles.web@gmail.com>
2017-05-11 16:23:15 -0500

    todos los casos

--- lectura.sensor.php
+++ lectura.sensor.php
@@ -4,19 +4,31 @@ if( !empty($_GET)) {
 	if (isset($_GET["id1"])){
 		$id1  = htmlspecialchars($_GET["id1"],ENT_QUOTES);
 		echo "id1 ha sido enviado exitosamente, su valor es: ".$id1."\n";
+	} else {
+		echo "no se ha encontrado a id1\n";
 	}
+
 	if (isset($_GET["id2"])){
 		$id1  = htmlspecialchars($_GET["id2"],ENT_QUOTES);
 		echo "id2 ha sido enviado exitosamente, su valor es: ".$id2."\n";
+	} else {
+		echo "no se ha encontrado a id2\n";
 	}
+	
 	if (isset($_GET["v1"])){
 		$id1  = htmlspecialchars($_GET["v1"],ENT_QUOTES);
 		echo "v1 ha sido enviado exitosamente, su valor es: ".$v1."\n";
+	} else {
+		echo "no se ha encontrado a v1\n";
 	}
+	
 	if (isset($_GET["v2"])){
 		$id1  = htmlspecialchars($_GET["v2"],ENT_QUOTES);
 		echo "v2 ha sido enviado exitosamente, su valor es: ".$v2."\n";
+	} else {
+		echo "no se ha encontrado a v2\n";
 	}
+	
 
 } else{
 	echo "No se ha recibido ningun valor";
