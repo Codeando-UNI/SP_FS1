461c465e21dc7eec1c02047e3b70abddea14ffa9
juan <jotabeles.web@gmail.com>
2017-05-11 18:43:12 -0500

    cambiando fin de carrera

--- lectura.sensor.php
+++ lectura.sensor.php
@@ -3,30 +3,30 @@
 if( !empty($_GET)) {
 	if (isset($_GET["id1"])){
 		$id1  = htmlspecialchars($_GET["id1"],ENT_QUOTES);
-		echo "id1 ha sido enviado exitosamente, su valor es: ".$id1."\n";
+		echo "id1 ha sido enviado exitosamente, su valor es: ".$id1;
 	} else {
-		echo "no se ha encontrado a id1\n";
+		echo "no se ha encontrado a id1";
 	}
 
 	if (isset($_GET["id2"])){
-		$id1  = htmlspecialchars($_GET["id2"],ENT_QUOTES);
-		echo "id2 ha sido enviado exitosamente, su valor es: ".$id2."\n";
+		$id2  = htmlspecialchars($_GET["id2"],ENT_QUOTES);
+		echo "id2 ha sido enviado exitosamente, su valor es: ".$id2;
 	} else {
-		echo "no se ha encontrado a id2\n";
+		echo "no se ha encontrado a id2";
 	}
 	
 	if (isset($_GET["v1"])){
-		$id1  = htmlspecialchars($_GET["v1"],ENT_QUOTES);
-		echo "v1 ha sido enviado exitosamente, su valor es: ".$v1."\n";
+		$v1  = htmlspecialchars($_GET["v1"],ENT_QUOTES);
+		echo "v1 ha sido enviado exitosamente, su valor es: ".$v1;
 	} else {
-		echo "no se ha encontrado a v1\n";
+		echo "no se ha encontrado a v1";
 	}
 	
 	if (isset($_GET["v2"])){
-		$id1  = htmlspecialchars($_GET["v2"],ENT_QUOTES);
-		echo "v2 ha sido enviado exitosamente, su valor es: ".$v2."\n";
+		$v2  = htmlspecialchars($_GET["v2"],ENT_QUOTES);
+		echo "v2 ha sido enviado exitosamente, su valor es: ".$v2;
 	} else {
-		echo "no se ha encontrado a v2\n";
+		echo "no se ha encontrado a v2";
 	}
 	
 
