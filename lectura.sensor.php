74441d4e2875d5f64da358f241449cb022617312
juan <jotabeles.web@gmail.com>
2017-05-11 18:46:41 -0500

    agregando espacios

--- lectura.sensor.php
+++ lectura.sensor.php
@@ -3,30 +3,30 @@
 if( !empty($_GET)) {
 	if (isset($_GET["id1"])){
 		$id1  = htmlspecialchars($_GET["id1"],ENT_QUOTES);
-		echo "id1 ha sido enviado exitosamente, su valor es: ".$id1;
+		echo "id1 ha sido enviado exitosamente, su valor es: ".$id1." ";
 	} else {
-		echo "no se ha encontrado a id1";
+		echo "no se ha encontrado a id1 ";
 	}
 
 	if (isset($_GET["id2"])){
 		$id2  = htmlspecialchars($_GET["id2"],ENT_QUOTES);
-		echo "id2 ha sido enviado exitosamente, su valor es: ".$id2;
+		echo "id2 ha sido enviado exitosamente, su valor es: ".$id2." ";
 	} else {
-		echo "no se ha encontrado a id2";
+		echo "no se ha encontrado a id2 ";
 	}
 	
 	if (isset($_GET["v1"])){
 		$v1  = htmlspecialchars($_GET["v1"],ENT_QUOTES);
-		echo "v1 ha sido enviado exitosamente, su valor es: ".$v1;
+		echo "v1 ha sido enviado exitosamente, su valor es: ".$v1." ";
 	} else {
-		echo "no se ha encontrado a v1";
+		echo "no se ha encontrado a v1 ";
 	}
 	
 	if (isset($_GET["v2"])){
 		$v2  = htmlspecialchars($_GET["v2"],ENT_QUOTES);
-		echo "v2 ha sido enviado exitosamente, su valor es: ".$v2;
+		echo "v2 ha sido enviado exitosamente, su valor es: ".$v2." ";
 	} else {
-		echo "no se ha encontrado a v2";
+		echo "no se ha encontrado a v2 ";
 	}
 	
 
