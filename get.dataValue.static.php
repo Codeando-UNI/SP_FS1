c05f97e1c9c951bc99182f9243361f2bcf68097f
Gherard <gachipana@gmail.com>
2017-05-16 14:21:35 -0500

    Cambios 2

--- get.dataValue.static.php
+++ get.dataValue.static.php
@@ -1,8 +1,8 @@
 
 <?php  
-$StaticInfo = array(
-	'medidas' =>[ [
-	'Id'=>1,
+$StaticInfo = array(	
+	'medidas' =>[ 
+	['Id'=>1,
 	'valor'=>rand(0,25),
 	'fecha'=>'12-05-2017'] ]
 	);
--- show.potenciometros.php
+++ show.potenciometros.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-<script type="text/javascript" languaje="javascript" src="js/jquery.js"></script>
+<script type="text/javascript" languaje="javascript" src="js/jquery-3.2.1.min.js"></script>
 
     <style>
       #map {
@@ -17,8 +17,11 @@
     <script>
     var zoom, latcenter, longcenter;
     var datos2;
+    var medidas2;
     var infowindow;
-      function rescatarDAtos(){
+
+
+      function rescatarDatos(){
       	$parametros = {
             
         };
@@ -39,37 +42,31 @@
                   initMap();             
             }
         });
+
       }
+
       function initMap() {
+       console.log(latcenter);
         var central={lat: latcenter, lng: longcenter};
+        
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: zoom,
           center: central
         });
+
+       infowindow = new google.maps.InfoWindow();
         for (var i = 0; i <= datos2.length - 1; i++) {
           var coordenadas={lat: datos2[i].latitude, lng: datos2[i].longitude};
           var marker = new google.maps.Marker({
             position: coordenadas,
             map: map,
-            title: datos2[i].Name
-          });  
+            title: datos2[i].Name,            
+            });  
 
-          
-        infowindow=  new google.maps.InfoWindow();
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                          return function() {
-                              consultar(datos2[i].Id,marker);
-                              
-                              //map.setCenter(marker.getPosition());       
-                                                    
-                          }
-            })(marker, i));
-        }
-      }
-      function consultar(Id,marker){
-        $parametros = {
-            'ID' : Id
-        };
+    
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+
         $.ajax({
             url: 'get.dataValue.static.php',
             type: 'POST',
@@ -79,21 +76,71 @@
             error: function(arguments,error){
               console.log(error);
             },
-            success: function (datos){
-              //alert(datos.medidas[0].valor);
-              infowindow.setContent(""+datos.medidas[0].valor);
-                infowindow.open(map, marker);
+            success: function (medidas){                
+                medidas2=medidas.medidas;                 
+                   infowindow.setContent("<b>Nombre:</b> "+datos2[i].Name+"<br><b>Valor:</b> "+medidas2[0].valor+"<br><b>Fecha:</b> "+medidas2[0].fecha);                   
+                    infowindow.open(map, marker);               
             }
-        });
+        });                                        
+                }
+            })(marker, i));
+
+        }
 
       }
 
+ function cargarDatosMapa (){
+        // Programa Ajax para pedir Data del Mapa
+        $parametros = {
+               
+            };
+        $url = "get.dataValue.static.php";
+        $.ajax({
+            type: "POST",
+            url: $url,
+            data: $parametros,
+            dataType : "json",
+            success: function(data){
+                ArrayData = data;
+                pointMarker = ArrayData.pointMarker;
+                mapOptions = {
+                  zoom: ArrayData.mapOption.zoom,
+                  center: new google.maps.LatLng(ArrayData.mapOption.centerPosition[0],ArrayData.mapOption.centerPosition[1]),
+                  mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+
+                rescatarDatos ();   
+                addMarkerWithTimeout();
+            }
+        });
+    }
+
+      function addMarkerWithTimeout(position, timeout) {
+        window.setTimeout(function() {
+          markers.push(new google.maps.Marker({
+            position: position,
+            map: map,
+            animation: google.maps.Animation.DROP
+          }));
+        }, timeout);
+      }
+
+
+
+      function consultar(){
+
+      };
+
+
 	window.onload = function(){
-	   rescatarDAtos();
+	   rescatarDatos();
      
     }
     </script>
-   <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyBLnlAQH441DxvN2xRinRYC3hyQ_5BGJGE" async="" defer="defer" type="text/javascript"></script>
+   <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyCUM-S9w4Bc67h8-z26N7f5oNSppl7TcTk" async="" defer="defer" type="text/javascript"></script>
   
   </body>
 </html>
+
+
+
