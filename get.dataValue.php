be6ed64f71fd46cf9b6db69e9555bccdeb8d0973
juan <jotabeles.web@gmail.com>
2017-05-16 16:23:26 -0500

    mis cambios jota

--- get.dataValue.php
+++ get.dataValue.php
@@ -4,19 +4,20 @@
 
 include 'conexion.php';
 $id="2";
+$id1  = htmlspecialchars($_GET["ID"],ENT_QUOTES);
 $query = "select * from medicion where id_potenciometro=".$id." order by id desc limit 0,1";
 $sad = mysqli_query($conecta, $query);
 $data = array();
 $mostrar =mysqli_fetch_array($sad);
 
-print_r($mostrar);
+//print_r($mostrar);
 
-//while ( $mostrar =mysqli_fetch_array($sad)) {
-//    $tmp = array("datos" => [
-//            "medicion" => $mostrar["value"],
-//                "fecha" => $mostrar["date"]
-//            ]);
-//    array_push($data, $tmp);
-//    
-//}
-//echo json_encode($data);
+while ( $mostrar =mysqli_fetch_array($sad)) {
+    $tmp = array("datos" => [
+            "medicion" => $mostrar["value"],
+                "fecha" => $mostrar["date"]
+            ]);
+    array_push($data, $tmp);
+    
+}
+echo json_encode($data);
