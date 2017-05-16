6d7b6189aad9db08de04b99146708da8a9719ac6
juan <jotabeles.web@gmail.com>
2017-05-16 16:50:44 -0500

    cambio de lectura.sensor.php

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 
 $dbhost = "localhost";
 $dbuser = "root";
-$dbpass = "123";
+$dbpass = "jibf123";
 $dbname = "sp_fs1";
 $conecta = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)
         or die(mysql_error("No se pudo  conectar a la base de datos"));
--- get.dataValue.php
+++ get.dataValue.php
@@ -8,9 +8,6 @@ $id1  = htmlspecialchars($_GET["ID"],ENT_QUOTES);
 $query = "select * from medicion where id_potenciometro=".$id." order by id desc limit 0,1";
 $sad = mysqli_query($conecta, $query);
 $data = array();
-$mostrar =mysqli_fetch_array($sad);
-
-//print_r($mostrar);
 
 while ( $mostrar =mysqli_fetch_array($sad)) {
     $tmp = array("datos" => [
