6ddc1f88b01317316a3d9b545c38430c06be4677
guscorpio96 <guscorpio96@gmail.com>
2017-05-16 15:05:59 -0500

    subida

--- show.potenciometros.php
+++ show.potenciometros.php
@@ -1,99 +1,30 @@
 <!DOCTYPE html>
 <html>
-  <head>
-<script type="text/javascript" languaje="javascript" src="js/jquery.js"></script>
+<head>
+	<title>Marcadore multiples v1.0
+	</title> 
 
+	
+<script src="//maps.googleapis.com/maps/api/js?key=AIzaSyBLnlAQH441DxvN2xRinRYC3hyQ_5BGJGE" async="" defer="defer" type="text/javascript"></script>
     <style>
       #map {
         height: 400px;
         width: 100%;
        }
     </style>
-  </head>
-  <body>
-    <h3>My Google Maps Demo</h3>
-    <div id="map"></div>
-    <div id="img"></div>
-    <script>
-    var zoom, latcenter, longcenter;
-    var datos2;
-    var infowindow;
-      function rescatarDAtos(){
-      	$parametros = {
-            
-        };
-        $.ajax({
-            url: 'get.dataParametros.php',
-            type: 'POST',
-            async: true,
-            dataType: 'json',
-            data: $parametros,
-            error: function(arguments,error){
-            	console.log(error);
-            },
-            success: function (datos){
-                zoom=datos.zoom;
-                latcenter=datos.latcenter;
-                longcenter=datos.longcenter;
-                datos2=datos.datos; 
-                  initMap();             
-            }
-        });
-      }
-      function initMap() {
-        var central={lat: latcenter, lng: longcenter};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: zoom,
-          center: central
-        });
-        for (var i = 0; i <= datos2.length - 1; i++) {
-          var coordenadas={lat: datos2[i].latitude, lng: datos2[i].longitude};
-          var marker = new google.maps.Marker({
-            position: coordenadas,
-            map: map,
-            title: datos2[i].Name
-          });  
 
-          
-        infowindow=  new google.maps.InfoWindow();
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                          return function() {
-                              consultar(datos2[i].Id,marker);
-                              
-                              //map.setCenter(marker.getPosition());       
-                                                    
-                          }
-            })(marker, i));
-        }
-      }
-      function consultar(Id,marker){
-        $parametros = {
-            'ID' : Id
-        };
-        $.ajax({
-            url: 'get.dataValue.static.php',
-            type: 'POST',
-            async: true,
-            dataType: 'json',
-            data: $parametros,
-            error: function(arguments,error){
-              console.log(error);
-            },
-            success: function (datos){
-              //alert(datos.medidas[0].valor);
-              infowindow.setContent(""+datos.medidas[0].valor);
-                infowindow.open(map, marker);
-            }
-        });
+</head>
+<body>
+  <div id="header" class="col-md-12" >
+  <!-- Navigation -->
+    <?php include_once("include/navbar.general.php"); ?>  
+  </div>
+<div id="map">
+	
+</div>
 
-      }
-
-	window.onload = function(){
-	   rescatarDAtos();
-     
-    }
-    </script>
-   <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyBLnlAQH441DxvN2xRinRYC3hyQ_5BGJGE" async="" defer="defer" type="text/javascript"></script>
-  
-  </body>
+<script type="text/javascript" languaje="javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="show.example/maps.multipleMark.js"></script>
+<script type="text/javascript" src="show.example/rescatar.datos.js"></script>
+</body>
 </html>
